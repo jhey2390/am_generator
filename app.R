1322e587f17a9ff9cd2d0936fad3949ce133beef
Jheynner Castro <jheynnerc@gmail.com>
2022-06-07 19:34:33 -0500

    se enter the commit message for your changes. Lines starting

--- app.R
+++ app.R
@@ -15,6 +15,10 @@ library(tidyverse)
 Sys.setlocale(locale = "es_ES.UTF-8")
 load("./CBD.rdata")
 load("./CBDF2.rdata")
+load("./data_consolidada.rdata")
+rm(dff2,programados)
+listado<-data_consolidada%>%select(codgeo=CODGEO,d_dpto=D_DPTO,d_prov=D_PROV,d_dist=D_DIST) %>% unique() %>% arrange(codgeo)
+rm(data_consolidada)
 googledrive::drive_auth(path = "your-app-folder-name/.secrets/calcium-chalice-291219-c0edbd3ab245.json")
 
 jp_folder <- "https://drive.google.com/drive/folders/1UjvV0CPbu3kQKLanWTDxRUoNdYsUehSL"
@@ -27,47 +31,64 @@ files<-files %>%
 
 file<-files %>% filter(fec==max(files$fec)) %>% head(1)
 
-
 # Define UI for application that draws a histogram
-ui <- (fluidPage(
+ui <- fluidPage(
     titlePanel("Generador de Ayudas Memorias de Cierre de Brecha Digital"),
     h4(paste("Información de distribución Fase 1 al ",format(as.Date(actualizacion),"%d-%m-%Y"),sep="")),
     h4(paste("Información de distribución Fase 2 al ",format(as.Date(fecha2),"%d-%m-%Y"),sep="")),
     h4(paste("Información de actualización de tabletas al ",format(as.Date(file$fec),"%d-%m-%Y"),sep="")),
     
-    
     selectInput("selection", 
                 h3("Ayuda Memoria a nivel:"),
-                list("Nacional", "Departamental")),
-    sidebarPanel (
+                list("Nacional", "Departamental","Provincial")),
+    sidebarLayout(
+      sidebarPanel(
         conditionalPanel(
             condition = "input.selection == 'Nacional'",
-            h5("Se imprimirá la AM a nivel Nacional.")
-        ),
-        conditionalPanel(
-            condition = "input.selection == 'Departamental'",
-            selectInput("Departamento", "Departamento:",
-                        list("Amazonas","Áncash","Apurímac","Arequipa","Ayacucho",
-                             "Cajamarca","Cusco","Huancavelica","Huánuco",
-                             "Ica","Junín","La Libertad","Lambayeque","Lima",
-                             "Loreto","Madre de Dios","Moquegua","Pasco","Piura",
-                             "Puno","San Martín","Tacna","Tumbes","Ucayali")),
-            textOutput("result")
-            
-        ),
+            h5("Se imprimirá la AM a nivel Nacional.")),
+        uiOutput("inputdpto"),
+        uiOutput("inputprov"),
+        textOutput("result"),
         downloadButton(outputId = "printAM",label = "Descargar la AM")),
-    
-    
-))
+      mainPanel()),
+    )
 
 # Define server logic required to draw a histogram
 server <- function(input, output,session) {
-    output$result <- renderText({
-        paste("Se imprimirá la AM del departamento de", input$Departamento)
+  
+  output$inputdpto<- renderUI({
+    if(input$selection %in% c('Departamental','Provincial'))
+      {selectInput(inputId = "Departamento", "Departamento:",choices = var_dpto(),multiple = F)}
     })
+  output$inputprov<- renderUI({
+    if(input$selection == 'Provincial')
+      {selectInput(inputId = "Provincia", "Provincia:",choices = var_prov(),multiple = F)}
+    })
+  dpto <- reactive({
+    if (is.null(input$Departamento)) sort(unique(listado$d_dpto)) else input$Departamento})
+  var_dpto <- reactive({
+    if(is.null(listado)){return()}
+    as.list(sort(unique(listado$d_dpto)))
+  })
+  
+  var_prov<- reactive({
+      filter(listado, d_dpto %in% as.character(dpto())) %>% 
+        pull(d_prov) %>% 
+        unique()
+    })
+    output$result <- renderText({
+      if(input$selection == "Departamental")
+        paste("Se imprimirá la AM del departamento de", input$Departamento)
+      else if(input$selection == "Provincial")
+        paste("Se imprimirá la AM de la provincia",input$Provincia,"del departamento de", input$Departamento)
+    })
+    
     output$printAM <- downloadHandler(
         content = function(file) {
-            params <- list(my_class = ifelse(input$selection=='Nacional',"Nacional",input$Departamento))
+            params <- list(my_class = input$selection,
+                           dpto = input$Departamento,
+                           prov = input$Provincia
+                           )
             rmarkdown::render(input = "./Plantilla_AM_CBD.Rmd", 
                               output_file = file,
                               output_dir = tempdir(),
@@ -76,7 +97,7 @@ server <- function(input, output,session) {
                               envir = new.env(parent = globalenv()),
                               encoding="UTF-8")
         },
-        filename = function() {paste(gsub(":","",Sys.time()), "AM_CBD",ifelse(input$selection=='Nacional',"Nacional",input$Departamento),".docx")}
+        filename = function() {paste(gsub(":","",Sys.time()),"AM_CBD",ifelse(input$selection=='Nacional',"Nacional",paste(input$Provincia,input$Departamento)),".docx")}
     )
     session$allowReconnect(TRUE)
 }
